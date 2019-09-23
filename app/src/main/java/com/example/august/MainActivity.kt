c5c959295ec492ab16aa44f07681894f997ed9fe
mark <1464089197@qq.com>
2019-09-23 17:26:02 +0800

    将listview的全部动作都添加了

--- app/src/main/java/com/example/august/MainActivity.kt
+++ app/src/main/java/com/example/august/MainActivity.kt
@@ -25,6 +25,15 @@ import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.widget.*
 import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.undo.UndoAdapter
 import java.util.*
+import android.widget.AdapterView
+import android.widget.Toast
+import com.nhaarman.listviewanimations.itemmanipulation.dragdrop.OnItemMovedListener
+
+import com.nhaarman.listviewanimations.ArrayAdapter
+
+
+
+
 
 
 
@@ -55,15 +64,16 @@ class MainActivity : AppCompatActivity() {
 
         mDynamicListView.enableDragAndDrop()//使能拖拽
         mDynamicListView.setDraggableManager(TouchViewDraggableManager(R.id.textView_receive_mail))//可以拖拽的位置
-        //mDynamicListView.setOnItemMovedListener(MyOnItemMovedListener(myAdapter))
-        //mDynamicListView.setOnItemLongClickListener(new MyOnItemLongClickListener(mDynamicListView))
+        mDynamicListView.setOnItemMovedListener(MyOnItemMovedListener(myAdapter as ArrayAdapter<MailItem>))//移动监听
+        mDynamicListView.setOnItemLongClickListener(MyOnItemLongClickListener(mDynamicListView))//长按操作
 
-        mDynamicListView.enableSimpleSwipeUndo()
+        mDynamicListView.enableSimpleSwipeUndo()//使能撤销
+        mDynamicListView.setOnItemClickListener(MyOnItemClickListener(mDynamicListView));
 
     }//onCreat
 
     //控制删除操作的类，OnDismissCallback是简单的删除处理，见官方教程
-    private class MyOnDismissCallback internal constructor(private val mAdapter: com.nhaarman.listviewanimations.ArrayAdapter<MailItem>) :
+    private inner class MyOnDismissCallback internal constructor(private val mAdapter: com.nhaarman.listviewanimations.ArrayAdapter<MailItem>) :
         OnDismissCallback {
         private var mToast: Toast? = null
         //这个方法在删除操作进行时会被调用
@@ -72,17 +82,71 @@ class MainActivity : AppCompatActivity() {
                 mAdapter.remove(position)//删除数据
             }
 
-//            if (mToast != null) {
-//                mToast!!.cancel()
-//            }
+            if (mToast != null) {
+                mToast!!.cancel()
+            }
+            mToast = Toast.makeText(
+                this@MainActivity,
+                "You have delete a mail address!",//被删除的时候显示这句提示
+                Toast.LENGTH_LONG
+            )
+            mToast!!.show()
+        }
+    }
+
+    //控制添加的类
+    private inner class MyOnItemClickListener internal constructor(private val mListView: DynamicListView) :
+        AdapterView.OnItemClickListener {
+
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            //item点击之后的操作在这里进行，下面这个例子是在点击的位置添加一个item
+            //mListView.insert(position, MailItem("new",true))
+        }
+    }
+
+    //控制移动的类
+    private inner class MyOnItemMovedListener internal constructor(private val mAdapter: ArrayAdapter<MailItem>) :
+        OnItemMovedListener {
+
+        private var mToast: Toast? = null
+
+        override fun onItemMoved(originalPosition: Int, newPosition: Int) {
+            if (mToast != null) {
+                mToast!!.cancel()
+            }
+            //当item被移动的时候执行的代码，下面是移动后显示moved的例子
+
 //            mToast = Toast.makeText(
 //                this@MainActivity,
-//                "You have delete a mail address! You can undo it now.",
-//                Toast.LENGTH_LONG
+//                "moved",
+//                Toast.LENGTH_SHORT
 //            )
 //            mToast!!.show()
         }
     }
+
+    //操作长按
+    private inner class MyOnItemLongClickListener internal constructor(val listview:DynamicListView?): AdapterView.OnItemLongClickListener{
+        val mListView:DynamicListView?
+
+        init {
+            mListView=listview
+        }
+
+        override fun onItemLongClick(
+            parent: AdapterView<*>?,
+            view: View?,
+            position: Int,
+            id: Long
+        ): Boolean {
+            if (mListView != null) {
+                //长按的操作，下面是长按拖动的例子
+                //mListView.startDragging(position - mListView.getHeaderViewsCount());
+            }
+            return true
+        }
+    }
+
 
 
 }
